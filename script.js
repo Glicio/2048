62658e798feb9c9ca9ac3b7535ab5f1a21d3d0df
Glicio <glicioo@outlook.com>
2022-03-09 22:57:57 -0300

    commit

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 console.log("script.js carregado!")
-import Grid from './Grid'
+import Grid from './Grid.js'
+import Tile from './Tile.js'
 
 const gameBoard = document.getElementById('game-board')
-console.log(gameBoard);
 const grid = new Grid(gameBoard)
+
+
+
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+
+setupInput()
+
+
+
+function setupInput() {
+    window.addEventListener("keydown", handleClick, {once: true})
+}
+
+function handleClick(e){
+ switch(e.key){
+    case "ArrowUp":
+        moveUp();
+        break;
+    case "ArrowDown":
+        moveDown();
+        break;
+    case "ArrowLeft":
+        moveLeft();
+        break;
+    case "ArrowRight":
+        moveRight();
+        break;
+    default:
+        setupInput();
+        break
+    }  
+
+    setupInput()
+}
+
+function moveUp(){
+    slideTiles(grid.cellsByColumn)
+}
